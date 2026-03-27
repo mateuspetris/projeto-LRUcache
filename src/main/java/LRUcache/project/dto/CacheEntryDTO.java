6d6f27b6499216877d4165e7d0f2745a8d2441e7
Mateusssi <mateuspetris@gmail.com>
2026-03-27 03:23:18 -0300

    fix: restaurando arquivos removidos acidentalmente

---
+++ src/main/java/LRUcache/project/dto/CacheEntryDTO.java
@@ -0,0 +1,13 @@
+package LRUcache.project.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CacheEntryDTO {
+    private String key;
+    private String value;
+    private String position;
+}
